79330ba2a94ffc911a70d9aa570b93dbffec98c4
Salah-Eddine <devmaarouf@gmail.com>
2025-02-28 18:00:12 +0000

    Add policy for frontend ocean

--- src/API/Ekygai.API/Program.cs
+++ src/API/Ekygai.API/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policyBuilder =>
     {
         // Adjust as needed: if your frontend is on http://localhost:3000, specify that
-        policyBuilder.WithOrigins("http://localhost:3000")
+        policyBuilder.WithOrigins("http://localhost:3000", "https://ekygai-bv6cw.ondigitalocean.app")
                      .AllowAnyMethod()
                      .AllowAnyHeader()
                      .AllowCredentials(); // If you need cookies/auth headers
